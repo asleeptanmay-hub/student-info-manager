49ebd26c87149b04bb683fb65832ff3d2dc3625f
ROBIN HOOD <asleeptanmay@gmail.com>
2026-06-18 17:44:51 +0530

    Add files via upload

---
+++ Student Information Manager.py
@@ -0,0 +1,11 @@
+student={"name":"tanmay",
+         "age": 18,
+         "marks": [90, 90, 56],
+         "passed": "true"
+         
+         }
+print("name:",student["name"])
+print("age:", student["age"])
+print("marks:", student["marks"])
+print("passed:", student["passed"])
+
